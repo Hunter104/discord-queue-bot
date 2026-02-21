bd3dbfce04c17bcc4efc8ce0a5bff4c33df623d7
Hunter104 <000plagueinc@gmail.com>
2026-02-21 00:49:40 -0300

    Formatação de comandos redis

--- server-c/valkey_funcs.c
+++ server-c/valkey_funcs.c
@@ -1,7 +1,6 @@
 #include "valkey_funcs.h"
 #include <hiredis/hiredis.h>
 #include <hiredis/read.h>
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +19,11 @@ const char *USER_ASSIGNMENT_SUFFIX = "assigned_to";
 
 int register_host(redisContext *c, char *hostname) {
   redisReply *reply = (redisReply *)redisCommand(
-      c, "SADD %s %s", REGISTERED_HOSTS_KEY, hostname);
+      c,
+      "SADD %s %s",
+      REGISTERED_HOSTS_KEY,
+      hostname
+      );
   if (reply == NULL) {
     freeReplyObject(reply);
     return 1;
@@ -32,7 +35,11 @@ int register_host(redisContext *c, char *hostname) {
 int pop_waiting_queue(redisContext *c, int timeout, char *str, size_t n) {
   int rc = 0;
   redisReply *reply = (redisReply *)redisCommand(
-      c, "BLMOVE %s %s LEFT RIGHT 0", WAITING_QUEUE_KEY, PROCESSING_QUEUE_KEY);
+      c,
+      "BLMOVE %s %s LEFT RIGHT 0",
+      WAITING_QUEUE_KEY,
+      PROCESSING_QUEUE_KEY
+      );
   CHECK_REPLY(c, reply, rc);
 
   strncpy(str, reply->str, n - 1);
@@ -40,8 +47,14 @@ int pop_waiting_queue(redisContext *c, int timeout, char *str, size_t n) {
   freeReplyObject(reply);
   reply = NULL;
 
-  reply = (redisReply *)redisCommand(c, "SET %s:%s:%s %s NX EX %d", USER_PREFIX,
-                                     str, USER_LOCK_SUFFIX, "locked", timeout);
+  reply = (redisReply *)redisCommand(
+    c,
+    "SET %s:%s:%s %s NX EX %d",
+    USER_PREFIX,
+    str,
+    USER_LOCK_SUFFIX,
+    "locked",
+    timeout);
   CHECK_REPLY(c, reply, rc);
 
 cleanup:
@@ -52,8 +65,13 @@ cleanup:
 
 int release_user(redisContext *c, char *username) {
   int rc = 0;
-  redisReply *reply = (redisReply *)redisCommand(c, "DEL %s:%s:%s", USER_PREFIX,
-                                                 username, USER_LOCK_SUFFIX);
+  redisReply *reply = (redisReply *)redisCommand(
+    c,
+    "DEL %s:%s:%s",
+    USER_PREFIX,
+    username,
+    USER_LOCK_SUFFIX
+    );
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
 cleanup:
@@ -64,27 +82,40 @@ cleanup:
 
 redisReply *set_status_command(redisContext *c, HostStatus hostStatus,
                                int timeout) {
-  redisReply *reply = (redisReply *)redisCommand(
-      c,
-      "HSETEX %s:%s:%s EX %d FIELDS 5 hostname %s is_occupied %d expiry "
-      "%lu current_user %s last_timestamp %lu",
-      HOST_PREFIX, hostStatus.hostname, HOST_STATUS_SUFFIX, timeout,
-      hostStatus.hostname, hostStatus.is_occupied, hostStatus.expiry,
-      hostStatus.current_user, hostStatus.last_timestamp);
-  return reply;
+  return redisCommand(
+    c,
+    "HSETEX %s:%s:%s EX %d "
+    "FIELDS 5 "
+    "hostname %s "
+    "is_occupied %d "
+    "expiry %lu "
+    "current_user %s "
+    "last_timestamp %lu",
+    HOST_PREFIX, hostStatus.hostname, HOST_STATUS_SUFFIX,
+    timeout,
+    hostStatus.hostname,
+    hostStatus.is_occupied,
+    hostStatus.expiry,
+    hostStatus.current_user,
+    hostStatus.last_timestamp
+  );
 }
 
 int set_status(redisContext *c, HostStatus hostStatus, int timeout) {
   int rc = 0;
-  redisReply *reply;
-  reply = redisCommand(c, "MULTI");
+  redisReply *reply = redisCommand(c, "MULTI");
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   freeReplyObject(reply);
   if (hostStatus.is_occupied) {
-    reply = redisCommand(c, "SET %s:%s:%s %s EX %d", USER_PREFIX,
-                         hostStatus.current_user, USER_ASSIGNMENT_SUFFIX,
-                         hostStatus.hostname, timeout);
+    reply = redisCommand(
+      c,
+      "SET %s:%s:%s %s EX %d",
+      USER_PREFIX,
+      hostStatus.current_user,
+      USER_ASSIGNMENT_SUFFIX,
+      hostStatus.hostname, timeout
+    );
     CHECK_AND_CLEAN_REPLY(c, reply, rc);
   }
   reply = set_status_command(c, hostStatus, timeout);
@@ -103,7 +134,12 @@ cleanup:
 int get_status(redisContext *c, char *hostname, HostStatus *hostStatus) {
   int rc = 0;
   redisReply *reply = (redisReply *)redisCommand(
-      c, "HGETALL %s:%s:%s", HOST_PREFIX, hostname, HOST_STATUS_SUFFIX);
+      c,
+      "HGETALL %s:%s:%s",
+      HOST_PREFIX,
+      hostname,
+      HOST_STATUS_SUFFIX
+      );
   CHECK_REPLY(c, reply, rc);
 
   if (reply->type == REDIS_REPLY_NIL || reply->elements == 0) {
@@ -120,7 +156,7 @@ int get_status(redisContext *c, char *hostname, HostStatus *hostStatus) {
   strncpy(hostStatus->hostname, reply->element[1]->str, MAX_NAME_SIZE - 1);
   hostStatus->hostname[MAX_NAME_SIZE - 1] = '\0';
 
-  hostStatus->is_occupied = atoi(reply->element[3]->str);
+  hostStatus->is_occupied = strtol(reply->element[3]->str, NULL, 10);
   hostStatus->expiry = strtoul(reply->element[5]->str, NULL, 10);
 
   strncpy(hostStatus->current_user, reply->element[7]->str, MAX_NAME_SIZE - 1);
@@ -140,6 +176,7 @@ cleanup:
 // Set user as assigned to host, update host status, push notification and clear
 // processing queue
 int finish_assignment(redisContext *c, HostStatus newStatus, int timeout) {
+  redisReply *reply = NULL;
   int rc = 0;
   if (!newStatus.is_occupied) {
     printf("Host %s is not occupied\n", newStatus.hostname);
@@ -148,13 +185,18 @@ int finish_assignment(redisContext *c, HostStatus newStatus, int timeout) {
   }
 
   // Open transaction
-  redisReply *reply = redisCommand(c, "MULTI");
+  reply = redisCommand(c, "MULTI");
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   // Update user status
-  reply = redisCommand(c, "SET %s:%s:%s %s EX %d", USER_PREFIX,
-                       newStatus.current_user, USER_ASSIGNMENT_SUFFIX,
-                       newStatus.hostname, timeout);
+  reply = redisCommand(
+    c,
+    "SET %s:%s:%s %s EX %d", USER_PREFIX,
+    newStatus.current_user,
+    USER_ASSIGNMENT_SUFFIX,
+    newStatus.hostname,
+    timeout
+    );
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   // Update host status
@@ -162,13 +204,22 @@ int finish_assignment(redisContext *c, HostStatus newStatus, int timeout) {
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   // Remove user from processing queu
-  reply = redisCommand(c, "LREM %s 0 %s", PROCESSING_QUEUE_KEY,
-                       newStatus.current_user);
+  reply = redisCommand(
+    c,
+    "LREM %s 0 %s",
+    PROCESSING_QUEUE_KEY,
+    newStatus.current_user
+    );
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   // Publish notification
-  reply = redisCommand(c, "PUBLISH %s:%s %s", HOST_PREFIX, newStatus.hostname,
-                       newStatus.current_user);
+  reply = redisCommand(
+    c,
+    "PUBLISH %s:%s %s",
+    HOST_PREFIX,
+    newStatus.hostname,
+    newStatus.current_user
+    );
   CHECK_AND_CLEAN_REPLY(c, reply, rc);
 
   // Commit transaction
